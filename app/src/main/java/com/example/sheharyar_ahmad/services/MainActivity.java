541b5e08b7bd57ef9c7fb1700d29116bd03a3562
sheharyar37 <31948192+sheharyar37@users.noreply.github.com>
2018-01-08 11:13:50 +0500

    Initial commit

---
+++ app/src/main/java/com/example/sheharyar_ahmad/services/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.sheharyar_ahmad.services;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button start, stop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        start = findViewById(R.id.start);
+        stop = findViewById(R.id.stop);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {startService(new Intent(MainActivity.this, MyService.class));
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(new Intent(MainActivity.this, MyService.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/sheharyar_ahmad/services/MyService.java
@@ -0,0 +1,37 @@
+package com.example.sheharyar_ahmad.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Sheharyar_Ahmad on 19/12/2017.
+ */
+
+public class MyService extends Service {
+    MediaPlayer player;
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        player = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
+        player.setLooping(true);
+        player.start();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        player.stop();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
